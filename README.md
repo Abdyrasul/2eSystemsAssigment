19d1c66c072cf9b0a3743336b127dcaf9dac944b
zkresul <abdyrasul@zksoftware.com.tr>
2022-03-05 14:40:05 +0300

    automated task

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 URL for published documentation
 https://documenter.getpostman.com/view/11222410/UVktoY1M
+
+###Reuirements
+JDK: 1.8.0_192\
+MYSQL: 8.0.28
+
+###Automated Task
+Parameters:\
+scheduling.fixedDelay(default=PT5S)\
+scheduling.enabled(default=false)\
+PT5S = every 5 seconds
+
+
+
+####Example:
+java -jar metar-1.0.jar scheduling.enabled=true scheduling.fixedDelay=PT5S
+
+You can find jar file inside the project folder
--- src/main/java/com/metar/MetarApplication.java
+++ src/main/java/com/metar/MetarApplication.java
@@ -6,11 +6,31 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Date;
+import java.util.Locale;
 
 @SpringBootApplication
 public class MetarApplication {
 
 	public static void main(String[] args) {
+		for (String arg : args){
+			String[] split = arg.split("=");
+			if(split.length>1){
+				String key = split[0];
+				String value = split[1];
+				switch (key){
+					case "scheduling.enabled":{
+						if(value.toLowerCase(Locale.ROOT).equals("true") || value.toLowerCase(Locale.ROOT).equals("false")){
+							System.setProperty("scheduling.enabled", value);
+						}
+					}
+					case "scheduling.fixedDelay": {
+						System.setProperty("scheduling.fixedDelay", value);
+						break;
+					}
+				}
+
+			}
+		}
 		SpringApplication.run(MetarApplication.class, args);
 	}
 
